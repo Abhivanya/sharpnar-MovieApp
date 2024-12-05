5aa0ce9cb97590a904511c9ff7ac35b9eec93493
Abhivanya <your_email@example.com>
2024-12-05 11:11:46 +0530

    added post and delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect, useCallback } from "react";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
+const URL = "https://movieapp-ed53d-default-rtdb.firebaseio.com/films.json";
+
 function App() {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
@@ -11,20 +13,22 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(URL);
       if (!response.ok) {
         throw new Error("Something went wrong");
       }
-      const data = await response.json();
 
-      const foramtedObject = data.results.map((data) => {
-        return {
-          id: data["episode_id"],
-          title: data.title,
-          releaseDate: data["release_date"],
-          openingText: data["opening_crawl"],
-        };
-      });
+      const data = await response.json();
+      const foramtedObject = [];
+      for (let key in data) {
+        foramtedObject.push({
+          id: key,
+          title: data[key].title,
+          releaseDate: data[key].releaseDate,
+          openingText: data[key].openingText,
+        });
+      }
+
       setDummyMovies(foramtedObject);
     } catch (err) {
       setError(err.message);
@@ -37,20 +41,56 @@ function App() {
     fetchData();
   }, [fetchData]);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const newData = {
-      id: new Date(),
       title: e.target.title.value,
       openingText: e.target.openingText.value,
       releaseDate: e.target.releaseDate.value,
     };
-    console.log(newData);
+
+    try {
+      setError(null);
+      const response = await fetch(URL, {
+        method: "POST",
+        body: JSON.stringify(newData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log(response);
+      alert("data added succefully");
+      fetchData();
+    } catch (err) {
+      setError(err.message);
+      console.log(err);
+    }
+    e.target.reset();
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      setError(null);
+      const response = await fetch(
+        `https://movieapp-ed53d-default-rtdb.firebaseio.com/films/${id}.json`,
+        {
+          method: "DELETE",
+        }
+      );
+      fetchData();
+    } catch (error) {
+      setError(error.message);
+      console.log(error);
+    }
   };
 
   let content;
+  if (!isLoading && !error && dummyMovies.length === 0) {
+    content = <p>No Data Found</p>;
+  }
+
   if (!isLoading && !error && dummyMovies.length > 0) {
-    content = <MoviesList movies={dummyMovies} />;
+    content = <MoviesList movies={dummyMovies} deleteMovie={handleDelete} />;
   }
   if (isLoading) {
     content = <p>Loading....</p>;
@@ -65,11 +105,11 @@ function App() {
       <section>
         <form onSubmit={handleSubmit}>
           <label>Title</label>
-          <input name="title" id="title" />
+          <input name="title" id="title" required />
           <label>Opening Text</label>
-          <textarea name="openingText"></textarea>
+          <textarea name="openingText" required></textarea>
           <label>Release Date</label>
-          <input name="releaseDate" />
+          <input name="releaseDate" required />
           <button>Add Movies</button>
         </form>
       </section>
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Movie.module.css';
+import classes from "./Movie.module.css";
 
 const Movie = (props) => {
   return (
     <li className={classes.movie}>
+      <span
+        className={classes.cancelBtn}
+        onClick={() => props.deleteMovie(props.id)}
+      >
+        x
+      </span>
       <h2>{props.title}</h2>
       <h3>{props.releaseDate}</h3>
       <p>{props.openingText}</p>
